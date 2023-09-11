af672a629f019ed19be296d0f0e588da08c7c7f6
YuriLealS <yuri.sapuppo@sptech.school>
2023-09-11 18:14:09 -0300

    Inicio do projeto

---
+++ app/src/main/java/sptech/school/lcsports/tela_cadastro.kt
@@ -0,0 +1,11 @@
+package sptech.school.lcsports
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela_cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_cadastro)
+    }
+}
