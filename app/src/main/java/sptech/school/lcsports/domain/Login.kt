a0d72b6edd136697581d948bea669aa3832e4e09
YuriLealS <yuri.sapuppo@sptech.school>
2023-11-21 17:48:57 -0300

    biblioteca okhttp3

--- app/src/main/java/sptech/school/lcsports/domain/Login.kt
+++
@@ -1,5 +0,0 @@
-package sptech.school.lcsports.domain
-data class Login (
-    var email:String? = null,
-    var senha:String? = null
-)
--- app/src/main/java/sptech/school/lcsports/login.kt
+++ app/src/main/java/sptech/school/lcsports/login.kt
@@ -4,19 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import retrofit2.Response
+import com.google.gson.Gson
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.json.JSONObject
 import sptech.school.lcsports.databinding.ActivityTelaLoginBinding
-import sptech.school.lcsports.domain.UsuarioLoginDto
-import sptech.school.lcsports.service.ApiService
-import retrofit2.Callback
-import retrofit2.Call
-import sptech.school.lcsports.domain.Login
-import sptech.school.lcsports.service.RetrofitClient
+import java.io.IOException
 
 class login : AppCompatActivity() {
 
-    private val apiService = RetrofitClient.instance
-    var isLogado = false
     val binding by lazy {
         ActivityTelaLoginBinding.inflate(layoutInflater)
     }
@@ -24,21 +22,71 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val authManager = AuthManager(this)
+
         binding.btLogar.setOnClickListener {
-//            val email = binding.etEmail.text.toString()
-//            val senha = binding.etSenha.text.toString()
-//            fazerAutenticacao(email,senha)
-//            if (isLogado) {
-//                Toast.makeText(baseContext, "Logado com sucesso!!", Toast.LENGTH_SHORT).show()
-//                val telaFeed = Intent(this, feed::class.java)
-//                startActivity(telaFeed)
-//            }
-//            else{
-//                Toast.makeText(baseContext, "Usuário/Senha inválidos", Toast.LENGTH_SHORT).show()
-//            }
-            Toast.makeText(baseContext, "Logado com sucesso!!", Toast.LENGTH_SHORT).show()
-                val telaFeed = Intent(this, feed::class.java)
-                startActivity(telaFeed)
+            if (binding.etEmail.text.toString().isEmpty()) {
+                binding.etEmail.error = "Preencha todos os campos"
+            } else if (binding.etSenha.text.toString().isEmpty()) {
+                binding.etSenha.error = "Preencha todos os campos"
+            } else if (binding.etSenha.text.toString().length < 6) {
+                binding.etSenha.error = "Senha inválida"
+            } else if (!binding.etEmail.text.toString().contains("@")) {
+                binding.etEmail.error = "Email inválido"
+            } else {
+                val email = binding.etEmail.text.toString()
+                val senha = binding.etSenha.text.toString()
+
+                val dadosLogin = JSONObject()
+                dadosLogin.put("email", email)
+                dadosLogin.put("senha", senha)
+
+                val client = OkHttpClient()
+                val url = "https://18.208.9.255/v1/usuarios/autenticar"
+                val json = dadosLogin.toString()
+                val body =
+                    RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json)
+                val request = Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build()
+
+                client.newCall(request).enqueue(object : okhttp3.Callback {
+                    override fun onFailure(call: okhttp3.Call, e: IOException) {
+                        runOnUiThread {
+                            Toast.makeText(baseContext, e.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                        val responseStatus = response.code()
+                        val responseBody = response.body()?.string()
+
+                        runOnUiThread {
+                            if (responseStatus == 404) {
+                                binding.etEmail.error = "Email não cadastrado"
+                            } else if (responseStatus == 401) {
+                                binding.etSenha.error = "Senha incorreta"
+                            } else if (response.isSuccessful) {
+                                if (responseBody != null) {
+                                    val gson = Gson()
+                                    val login = gson.fromJson(responseBody, DtoAuth::class.java)
+                                    authManager.saveAuthToken(login.token.toString())
+                                    val intent = Intent(this@login, feed::class.java)
+                                    startActivity(intent)
+                                }
+                            } else {
+                                Toast.makeText(
+                                    baseContext,
+                                    "Erro na resposta, tente novamente mais tarde",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                println(dadosLogin)
+                            }
+                        }
+                    }
+                })
+            }
         }
 
         binding.txtCadastro.setOnClickListener {
@@ -47,26 +95,4 @@ class login : AppCompatActivity() {
             startActivity(telaCadastro)
         }
     }
-        fun fazerAutenticacao(email: String, senha: String) {
-            val usuario = Login(email, senha)
-
-            val call = apiService.autenticar(usuario)
-            call.enqueue(object :Callback<UsuarioLoginDto> {
-                override fun onResponse(call: Call<UsuarioLoginDto>, response: Response<UsuarioLoginDto>) {
-                    if (response.isSuccessful) {
-                        val usuarioTokenDto = response.body()
-                        val nome = usuarioTokenDto?.nome
-                        isLogado = true
-                    } else {
-                        if (response.code() == 404) {
-                        } else if (response.code() == 401) {
-                        }
-                    }
                 }
-
-                override fun onFailure(call: Call<UsuarioLoginDto>, t: Throwable) {
-                    TODO("Not yet implemented")
-                }
-            })
-        }
-    }
