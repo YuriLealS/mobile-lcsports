4aea8bc4dcb2c56b962169a9482c83707a13daf6
YuriLealS <yuri.sapuppo@sptech.school>
2023-12-01 20:26:05 -0300

    Telas de perfil

---
+++ app/src/main/java/sptech/school/lcsports/criar_postagem.kt
@@ -0,0 +1,11 @@
+package sptech.school.lcsports
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class criar_postagem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_postagem)
+    }
+}
