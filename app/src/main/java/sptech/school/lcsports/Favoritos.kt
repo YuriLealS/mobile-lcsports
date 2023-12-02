064667e6732534e1d9cc5c89d6b3c82cb0fc4671
YuriLealS <yuri.sapuppo@sptech.school>
2023-12-02 16:01:01 -0300

    Ajustando crash do cadastro e linkando todas as telas para navegação

---
+++ app/src/main/java/sptech/school/lcsports/Favoritos.kt
@@ -0,0 +1,67 @@
+package sptech.school.lcsports
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import sptech.school.lcsports.databinding.ActivityFavoritosBinding
+
+class Favoritos : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityFavoritosBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btHome.setOnClickListener{
+            Toast.makeText(baseContext, "Home", Toast.LENGTH_SHORT).show()
+            val telaHome = Intent(this, Feed::class.java)
+            startActivity(telaHome)
+        }
+
+        binding.btLupa.setOnClickListener {
+            Toast.makeText(baseContext, "Categorias", Toast.LENGTH_SHORT).show()
+            val telaCategorias = Intent(this, Categorias::class.java)
+            startActivity(telaCategorias)
+        }
+
+        binding.img1.setOnClickListener {
+            Toast.makeText(baseContext, "Postagem", Toast.LENGTH_SHORT).show()
+            val telaPostagem = Intent(this, Postagem::class.java)
+            startActivity(telaPostagem)
+        }
+
+        binding.img2.setOnClickListener {
+            Toast.makeText(baseContext, "Postagem", Toast.LENGTH_SHORT).show()
+            val telaPostagem = Intent(this, Postagem::class.java)
+            startActivity(telaPostagem)
+        }
+
+        binding.img3.setOnClickListener {
+            Toast.makeText(baseContext, "Postagem", Toast.LENGTH_SHORT).show()
+            val telaPostagem = Intent(this, Postagem::class.java)
+            startActivity(telaPostagem)
+        }
+
+        binding.btFavoritos.setOnClickListener{
+            Toast.makeText(baseContext, "Favoritos", Toast.LENGTH_SHORT).show()
+            val telaFavoritos = Intent(this, Favoritos::class.java)
+            startActivity(telaFavoritos)
+        }
+
+        binding.btPublicar.setOnClickListener {
+            Toast.makeText(baseContext, "Postar", Toast.LENGTH_SHORT).show()
+            val telaCriarPostagem = Intent(this, CriarPostagem::class.java)
+            startActivity(telaCriarPostagem)
+        }
+
+        binding.btPerfil.setOnClickListener {
+            Toast.makeText(baseContext, "MeuPerfil", Toast.LENGTH_SHORT).show()
+            val telaMeuPerfil = Intent(this, MeuPerfil::class.java)
+            startActivity(telaMeuPerfil)
+        }
+
+    }
+}
---
+++ app/src/main/java/sptech/school/lcsports/MeuPerfil.kt
@@ -0,0 +1,46 @@
+package sptech.school.lcsports
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import sptech.school.lcsports.databinding.ActivityMeuPerfilBinding
+
+class MeuPerfil : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityMeuPerfilBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btHome.setOnClickListener{
+            Toast.makeText(baseContext, "Home", Toast.LENGTH_SHORT).show()
+            val telaHome = Intent(this, Feed::class.java)
+            startActivity(telaHome)
+        }
+
+        binding.btLupa.setOnClickListener {
+            Toast.makeText(baseContext, "Categorias", Toast.LENGTH_SHORT).show()
+            val telaCategorias = Intent(this, Categorias::class.java)
+            startActivity(telaCategorias)
+        }
+
+        binding.btFavoritos.setOnClickListener{
+            Toast.makeText(baseContext, "Favoritos", Toast.LENGTH_SHORT).show()
+            val telaFavoritos = Intent(this, Favoritos::class.java)
+            startActivity(telaFavoritos)
+        }
+
+        binding.btPublicar.setOnClickListener {
+            Toast.makeText(baseContext, "Postar", Toast.LENGTH_SHORT).show()
+            val telaCriarPostagem = Intent(this, CriarPostagem::class.java)
+            startActivity(telaCriarPostagem)
+        }
+
+        binding.btPerfil.setOnClickListener {
+            Toast.makeText(baseContext, "MeuPerfil", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
