573b7edc9b1686421a373f4e1c29e4b26878c609
YuriLealS <yuri.sapuppo@sptech.school>
2023-12-04 20:57:54 -0300

    Finalização das telas

---
+++ app/src/main/java/sptech/school/lcsports/FormaDePagamento.kt
@@ -0,0 +1,11 @@
+package sptech.school.lcsports
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormaDePagamento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forma_de_pagamento)
+    }
+}
---
+++ app/src/main/java/sptech/school/lcsports/SerPremium.kt
@@ -0,0 +1,11 @@
+package sptech.school.lcsports
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SerPremium : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ser_premium)
+    }
+}
